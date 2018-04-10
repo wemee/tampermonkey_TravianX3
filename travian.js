d2b2d14d5684da26cf0202c1619f57d8e5f62c4f
Isaac Tsai <isaac@adup.com.tw>
2018-04-10 15:46:54 +0800

    第三版

--- travian.js
+++ travian.js
@@ -13,38 +13,45 @@
 
   var host = 'https://ts20.travian.tw/';
 
-  function find_min_resourse_index(){
-    if($('#stockBarFreeCrop').text() < 10)
-      return 4;
-
-    var wood = $('#l1').text(),
-        clay = $('#l2').text(),
-        iron = $('#l3').text(),
-        crop = $('#l4').text();
-    var minRes = Math.min(wood, clay, iron, crop);
-    if(wood == minRes)
-      return 1;
-    else if(clay == minRes)
-      return 2;
-    else if(iron == minRes)
-      return 3;
-    else if(crop == minRes)
-      return 4;
-    else
-      return 2;
+  function find_min_resourse_index(func){
+    if($('#village_map > div.gid4.good.level0').length>=1){
+      func(4);
+    } else if($('#stockBarFreeCrop').text() < 10) {
+      console.log('stockBarFreeCrop < 10');
+      func(4);
+    } else {
+      var wood = $('#l1').text(), clay = $('#l2').text(),
+          iron = $('#l3').text(), crop = $('#l4').text();
+      var minRes = Math.min(wood, clay, iron, crop);
+      if(wood == minRes)
+        func(1);
+      else if(clay == minRes)
+        func(2);
+      else if(iron == minRes)
+        func(3);
+      else if(crop == minRes)
+        func(4);
+      else
+        func(2);
+    }
   }
 
   setInterval(function(){
     if(window.location.pathname == '/dorf1.php'){
-      var min_resourse_index = find_min_resourse_index();
-      var min_resourse_html_objs = $('#village_map > div.gid'+min_resourse_index+'.good');
-      if(min_resourse_html_objs.length>=1) {
-        console.log('goto build: '+($('#village_map > div.gid4.good:eq(0)').index()+1));
-        window.location.href = host+'build.php?id='+($('#village_map > div.gid4.good:eq(0)').index()+1);
-      } else {
-        console.log('not goto build');
-        location.reload();
-      }
+      find_min_resourse_index(function(min_resourse_index){
+        console.log('min_resourse_index:' +min_resourse_index);
+        var min_resourse_html_objs = $('#village_map > div.gid'+min_resourse_index+'.good');
+        if(min_resourse_html_objs.hasClass('level10')){
+          console.log('超過level 10');
+          return ;
+        } else if(min_resourse_html_objs.length>=1) {
+          console.log('goto build: '+(min_resourse_html_objs.index()+1));
+          window.location.href = host+'build.php?id='+(min_resourse_html_objs.index()+1);
+        } else {
+          console.log('not goto build');
+          location.reload();
+        }
+      });
     } else if(window.location.pathname == '/build.php'){
       var upgrade_btn = $('#build .section1 > button.green:not(.gold)');
       if(upgrade_btn.length >= 1){
@@ -58,5 +65,5 @@
       console.log('nothing: '+window.location.href);
       window.location.href = host+'dorf1.php';
     }
-  }, 1000*60);
+  }, window.location.pathname=='/build.php' ? 10000 : 60000);
 })();
